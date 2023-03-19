28491aa47708c712eb04118370a9e091b97d3e18
chetan <chetanbhagat9168@gmail.com>
2023-03-19 14:01:30 +0530

    update

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express=require("express");
 
 const app=express();
 app.get("/",(req,res)=>{
-    res.send("WELCOME")
+    res.send("WELCOME  Server 1")
 })
 app.listen (9168,()=>{
 console.log("server is running")
